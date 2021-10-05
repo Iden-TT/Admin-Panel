7be73969e2a81a2b0e2faf4e4d9c78eb32eb41d8
Saurav Sonu <sauravvsonu@gmail.com>
2021-10-05 15:42:30 +0530

    table update

--- assets/js/pending-fb.js
+++ assets/js/pending-fb.js
@@ -28,59 +28,144 @@ function addDataFb(data) {
 }
 
 function readData() {
-  //   var leadsRef = database.ref('users');
-  // leadsRef.on('value', function(snapshot) {
-  //   console.log(snapshot);
-  //     snapshot.forEach(function(childSnapshot) {
-  //       var childData = childSnapshot.val();
-  //       console.log(childData);
-  //     });
-  // });
-  // db.collection('users').get().then((snapshot) => {
-  //     console.log(snapshot);
-  //     snapshot.forEach(doc => {
-  //         let items = doc.data();
+  tableData = [];
 
-  //         /* Make data suitable for rendering */
-  //         items = JSON.stringify(items);
-
-  //         /* Update the components state with query result */
-  //         // this.setState({ items : items })
-  //     });
-
-  //   });
-  //   db.collection("users")
-  //     .get()
-  //     .then((doc) => {
-  //         console.log(doc.data());
-  //     //   if (doc) {
-  //     //     console.log("Document data:", doc.data());
-  //     //   } else {
-  //     //     // doc.data() will be undefined in this case
-  //     //     console.log("No such document!");
-  //     //   }
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error getting document:", error);
-  //     });
   db.collection("data")
     .get()
     .then((querySnapshot) => {
-      console.log(querySnapshot.data());
+      console.log(querySnapshot.docs);
       querySnapshot.forEach((doc) => {
-        console.log(doc.data());
+        tableData.push(doc.data());
+        // console.log(doc.data());
         let items = doc.data();
         items = JSON.stringify(items);
-        console.log(`${doc.id} => ${items}`);
+        // console.log(`${doc.id} => ${items}`);
       });
+      console.log(tableData);
+      updateTableHTML(tableData);
     });
+  // update = [
+  //   [1, "saurav sonu", "095564564665", "bank form", "view"],
+  //   [2, "saurav sonu", "095564564665", "bank form", "view"],
+  //   [3, "saurav sonu", "095564564665", "bank form", "view"],
+  //   [4, "saurav sonu", "095564564665", "bank form", "view"],
+  //   [5, "saurav sonu", "095564564665", "bank form", "view"],
+  // ];
+
+  // function updateTableHTML(myArray) {
+  //   // maps the list onto the existing table
+  //   var tableBody = document.getElementById("your-table-id"),
+  //     newRow,
+  //     newCell;
+
+  //   // Reset the table
+  //   tableBody.innerHTML = "";
+
+  //   // Build the new table
+  //   for (var i = 0; i < myArray.length; i++) {
+  //     newRow = document.createElement("tr");
+  //     tableBody.appendChild(newRow);
+
+  //     if (myArray[i] instanceof Array) {
+  //       for (var j = 0; j < myArray[i].length; j++) {
+  //         newCell = document.createElement("td");
+  //         newCell.textContent = update[i][j];
+  //         newRow.appendChild(newCell);
+  //       }
+  //     } else {
+  //       newCell = document.createElement("td");
+  //       newCell.textContent = myArray[i];
+  //       newRow.appendChild(newCell);
+  //     }
+  //   }
+  // }
+  update =[ 
+    {
+    name: "saurav",
+    phone: 2039509235,
+    type: "bank form",
+    action: "view"
+  },
+    {
+    name: "saurav",
+    phone: 2039509235,
+    type: "bank form",
+    action: "view"
+  },
+    {
+    name: "saurav",
+    phone: 2039509235,
+    type: "bank form",
+    action: "view"
+  },
+    {
+    name: "saurav sone",
+    phone: 2009235,
+    type: "bank form",
+    action: "view"
+  },
+    {
+    name: "saurav monkey",
+    phone: 2039509235,
+    type: "bank form",
+    action: "view"
+  },
+  ]
+
+  // updateTableHTML(update);
+  console.log(update);
+  console.log(updateTableHTML);
 }
 
 function gameChange(params) {
   console.log(db);
   addDataFb();
 }
+
 function gameChange2(params) {
   console.log(db);
   readData();
 }
+
+// function updateTableHTML(myArray) {
+//   console.log("this function called");
+//   // maps the list onto the existing table
+//   var tableBody = document.getElementById("your-table-id"),
+//     newRow,
+//     newCell;
+
+//     console.log(tableBody,"table body");
+
+//   // Reset the table
+//   tableBody.innerHTML = "";
+
+//   // Build the new table
+//   for (var i = 1; i <= myArray.length; i++) {
+//     newRow = document.createElement("tr");
+//     tableBody.appendChild(newRow);
+//     sno = document.createElement("td");
+//     names = document.createElement("td");
+//     phone = document.createElement("td");
+//     type = document.createElement("td");
+//     action = document.createElement("td");
+
+//     sno.textContent = i;
+//     names.textContent = myArray[i - 1].name;
+//     names.classlist = "name";
+//     phone.textContent = myArray[i - 1].phone;
+//     phone.classlist = "phone";
+//     type.textContent = myArray[i - 1].requested;
+//     type.classlist = "type";
+//     ata = document.createElement("a");
+//     ata.textContent = myArray[i - 1].active;
+//     ata.style.cursor = "pointer";
+//     ata.setAttribute("onclick", "viewreq()");
+
+//     newRow.appendChild(sno);
+//     newRow.appendChild(names);
+//     newRow.appendChild(phone);
+//     newRow.appendChild(type);
+//     newRow.appendChild(action);
+//     action.appendChild(ata);
+//   }
+// }
